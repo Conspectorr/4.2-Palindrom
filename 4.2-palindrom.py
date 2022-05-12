3602f621607cf80aee7f157e58cfcdf88bb2fa84
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-12 11:53:51 +0200

    Chech lenght word

--- 4.2-palindrom.py
+++ 4.2-palindrom.py
@@ -1,8 +1,15 @@
+from operator import length_hint
+
+
 def palindrom(word):
     """ 
-        Sprawdza, czy słowo jest palindromem.
+        Sprawdza, czy wyraz jest palindromem.
         Argument:
         word
     """
+    if len(word) % 2 ==0:
+        print("Wyraz nie jest palindromem.")
+    else:
+        print("Wyraz ma nieparzystą ilość liter.")
 
-help (palindrom)
+
